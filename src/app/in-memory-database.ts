ee89a1284e2b1c081514b4a29ea64c308564204a
Diogenes <diogenes@cellularsolution.com.br>
2020-01-20 21:30:57 -0300

    intercepting http requqests with angular-in-memory-web-api

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,14 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+import { Pendency } from "./pages/categories/shared/pendency.model";
+
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(){
+        const pendencies: Pendency[] = [
+            {id: 1, nome: "Diogenes", descricao: "Escrever o nome Camila na Mandala", corDoBanho: "dourado" },
+            {id: 1, nome: "Diogenes", descricao: "Escrever o nome Camila na Mandala", corDoBanho: "dourado" },
+        ];
+
+        return { pendencies }
+    }
+}
